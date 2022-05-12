ff0690e704d4873726a8ce124a6f76dc4e99dc68
adaahmetyasin <adaahmetyasinedu@gmail.com>
2022-05-12 16:01:19 +0300

    example

---
+++ array_test.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main(){
+
+
+//---------------------------------------------
+    // double scores[10] = {12, 23.5, 36};
+    // double x = scores[0];
+    // cout<<x<<endl;
+
+//---------------------------------------------
+
+
+
+    // const int CAPACITY = 10;
+    // int numbers[CAPACITY];
+    // int size;
+    // do{
+    //     cout << "Enter the size (1-10)" << endl;
+    //     cin >> size;
+    // }while (size < 1 || size > CAPACITY);
+
+    // cout << "Enter " << size << " integer(s): " ;
+
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cin >> numbers[i];
+    // }
+    // cout << "Numbers:" << endl;
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cout << numbers[i] << " ";
+    // }
+    // cout << endl << "Reverse Numbers:" << endl;
+    // for (int i = size - 1; i >= 0; i--)
+    // {
+    //     cout << numbers[i] << " ";
+    // }
+
+//------------------------------------------------- 
+    
+    
+    
+
+
+    return 0;
+}
---
+++ references.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // int number = 100;
+    // int& rNumber = number;
+
+    // number = 150;
+    // cout << number << endl;
+    // cout << rNumber << endl;
+
+
+
+    return 0;
+}
